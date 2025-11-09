f0a8644bebc2af7e32b4e738f67e57d4d36cc77e
Oleg'Ease'Kharchuk <olegease@gmail.com>
2025-11-09 07:04:46 +0200

    progress impl

--- include/son8/impl/insignia.hxx
+++ include/son8/impl/insignia.hxx
@@ -1,7 +1,18 @@
 #ifndef SON8_IMPL_INSIGNIA_HXX
 #define SON8_IMPL_INSIGNIA_HXX
 
+#include <son8/insignia.hxx>
+
 namespace son8::insignia {
+
+    template< typename Type >
+    auto Creator< Type >::operator()( ) const -> Type * { return new Type; }
+
+    template< typename Type >
+    auto Creator< Type >::operator()( Type const &data ) const -> Type * { return new Type{ data }; }
+
+    template< typename Type >
+    auto Creator< Type >::operator()( Type *data ) const { delete data; }
 
 }
 
